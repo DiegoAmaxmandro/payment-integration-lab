aa0fe2c7afaf7be9c8ca87e7122879f650189465
DiegoAmaxmandro <102663353+DiegoAmaxmandro@users.noreply.github.com>
2026-05-02 18:30:29 +0100

    Add XML auth transaction integration with Realex sandbox

--- integrations/xml/xml_client.py
+++ integrations/xml/xml_client.py
@@ -4,7 +4,7 @@ import datetime
 import os
 import sys
 
-# allow import from merchant_system
+# Allow importing config loader
 sys.path.append(
     os.path.abspath(
         os.path.join(os.path.dirname(__file__), "../../")
@@ -15,22 +15,16 @@ from merchant_system.config_loader import load_credentials
 
 
 def generate_timestamp():
-
     return datetime.datetime.now().strftime("%Y%m%d%H%M%S")
 
 
 def generate_hash(timestamp, merchant_id, order_id, amount, currency, secret):
 
-    data = (
-        timestamp +
-        merchant_id +
-        order_id +
-        amount +
-        currency
-    )
-
+    # First hash
+    data = timestamp + merchant_id + order_id + amount + currency
     hash1 = hashlib.sha1(data.encode()).hexdigest()
 
+    # Final hash
     final_hash = hashlib.sha1(
         (hash1 + secret).encode()
     ).hexdigest()
@@ -38,7 +32,7 @@ def generate_hash(timestamp, merchant_id, order_id, amount, currency, secret):
     return final_hash
 
 
-def create_xml_request():
+def build_xml():
 
     creds = load_credentials()
 
@@ -46,8 +40,7 @@ def create_xml_request():
 
     order_id = "ORDER123"
 
-    amount = "100"
-
+    amount = "55000"  # 100 = €1.00
     currency = "EUR"
 
     hash_value = generate_hash(
@@ -60,15 +53,31 @@ def create_xml_request():
     )
 
     xml = f"""
-    <request type="auth">
-        <merchantid>{creds["merchant_id"]}</merchantid>
-        <account>{creds["account_id"]}</account>
-        <orderid>{order_id}</orderid>
-        <amount currency="{currency}">{amount}</amount>
-        <timestamp>{timestamp}</timestamp>
-        <sha1hash>{hash_value}</sha1hash>
-    </request>
-    """
+<request type="auth">
+    <merchantid>{creds["merchant_id"]}</merchantid>
+    <account>{creds["account_id"]}</account>
+
+    <orderid>{order_id}</orderid>
+
+    <amount currency="{currency}">{amount}</amount>
+
+    <timestamp>{timestamp}</timestamp>
+
+    <card>
+        <number>4111111111111111</number>
+        <expdate>1225</expdate>
+        <chname>Test User</chname>
+        <cvn>
+            <number>123</number>
+            <presind>1</presind>
+        </cvn>
+    </card>
+
+    <autosettle flag="0"/>
+
+    <sha1hash>{hash_value}</sha1hash>
+</request>
+"""
 
     return xml
 
@@ -79,9 +88,9 @@ def send_request():
 
     url = creds["endpoint"]
 
-    xml_payload = create_xml_request()
+    xml_payload = build_xml()
 
-    print("Sending XML request to:")
+    print("\nSending request to:")
     print(url)
 
     response = requests.post(
@@ -93,8 +102,9 @@ def send_request():
         timeout=30
     )
 
-    print("Status Code:", response.status_code)
-    print("Response:")
+    print("\nStatus Code:", response.status_code)
+
+    print("\nGateway Response:\n")
     print(response.text)
 
 
