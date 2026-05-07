7582ea5ff03df5880f5956558847d6694ece740a
LEMOS <LEMOSDIE@globalpay.com>
2026-05-07 15:16:09 +0100

    Add initial XML transaction scenarios structure

---
+++ integrations/xml/scenarios.py
@@ -0,0 +1,11 @@
+SCENARIOS{
+    "success" : {
+        "order_id" : "ORDER131",
+        "amount" : "33000",
+        "currency" : "EUR",
+        "card_number" : "4263970000005262",
+        "expdate" : "1230",
+        "cvn" : "125",
+        "card_type" : "VISA"
+    }
+}
